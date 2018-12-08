de574660aa7e143f45b811685b48129360b9b877
luofx <luofxchn@outlook.com>
2018-12-08 21:49:08 +0800

    重置目錄

--- app/src/main/java/com/example/max/smarttoothbrush/Controller/MainActivity.java
+++ app/src/main/java/com/example/max/smarttoothbrush/Controller/MainActivity.java
@@ -1,14 +1,17 @@
-package com.example.max.smarttoothbrush;
+package com.example.max.smarttoothbrush.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ThirdActivity extends AppCompatActivity {
+import com.example.max.smarttoothbrush.R;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.third_layout);
+        setContentView(R.layout.main_layout);
+
     }
 }
---
+++ app/src/main/java/com/example/max/smarttoothbrush/Model/MainModel.java
@@ -0,0 +1,4 @@
+package com.example.max.smarttoothbrush.Model;
+
+public class MainModel {
+}
